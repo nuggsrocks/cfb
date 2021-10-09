b3494a348a6e5ac7a4fd1db556ae0bfddaca103d
mattfisher <nuggsrocks@yahoo.com>
2021-10-09 04:55:13 -0400

    sorting through data

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import math
+import re
 
 import pandas as pd
-
-import numpy as np
-
-import matplotlib.pyplot as plt
-
-from sklearn import linear_model
-
-from sklearn.model_selection import train_test_split
 
 import json
 
@@ -39,135 +31,42 @@ def get_team_name(team_id):
     return None
 
 
-roster_df = pd.DataFrame()
-schedule_df = pd.DataFrame()
-stats_df = pd.DataFrame()
+for team_id in stats:
+    team_stats = {'off': [], 'def': []}
+    for game_id in stats[team_id]:
+        plays = stats[team_id][game_id]
 
-for team_id, team_roster in roster.items():
-    team_roster_df = pd.DataFrame(data=team_roster)
+        plays_to_delete = []
 
-    team_roster_df['team_name'] = get_team_name(team_id)
-
-    roster_df = roster_df.append(team_roster_df, ignore_index=True)
-
-for team_id, team_schedule in schedule.items():
-    team_schedule_df = pd.DataFrame(data=team_schedule)
-
-    team_schedule_df['team_name'] = get_team_name(team_id)
-
-    schedule_df = schedule_df.append(team_schedule_df, ignore_index=True)
-
-for team_id, team_stats in stats.items():
-    stats_dict = {}
-    for game_index in range(len(team_stats)):
-        for key in team_stats[game_index]:
-            stats_dict[game_index, key] = team_stats[game_index][key]
-
-    team_stats_df = pd.DataFrame(stats_dict)
-
-    team_stats_df = team_stats_df.T
-
-    team_stats_df['team_name'] = get_team_name(team_id)
-
-    stats_df = stats_df.append(team_stats_df)
-
-
-def set_stat_dtype(series):
-    if series.name == 'time_of_possession':
-        def convert_to_seconds(x):
-            split = x.split(':')
-            minutes = int(split[0])
-            seconds = int(split[1])
-            return minutes * 60 + seconds
-
-        return series.apply(convert_to_seconds)
-    else:
-        try:
-            return series.astype('int')
-        except ValueError:
+        for play in plays:
             try:
-                return series.astype('float')
-            except ValueError:
-                return series.astype('string')
+                play['type'] = play['type']['text']
+            except KeyError:
+                plays_to_delete.append(play)
+                continue
 
+            if re.search('End of', play['type']) is not None:
+                plays_to_delete.append(play)
 
-stats_df = stats_df.apply(set_stat_dtype)
+        for play in plays_to_delete:
+            plays.remove(play)
 
-stats_df['rush_ypa'] = stats_df['rush_yards'] / stats_df['rush_att']
-stats_df['pass_ypa'] = stats_df['pass_yards'] / stats_df['pass_att']
-stats_df['plays'] = stats_df['rush_att'] + stats_df['pass_att']
-stats_df['yards_per_play'] = stats_df['total_yards'] / stats_df['plays']
-stats_df['3rd_down_%'] = stats_df['3rd_downs'] / stats_df['3rd_down_att']
+        for play in plays:
 
-team_df = stats_df.xs('team', level=1)
-opp_df = stats_df.xs('opp', level=1)
+            play['period'] = play['period']['number']
+            play['start_yard_line'] = play['start']['yardLine']
+            play['end_yard_line'] = play['end']['yardLine']
 
+            if play['type'] == 'Kickoff':
+                play['down'] = 'K'
+            else:
+                play['down'] = play['start']['down']
 
-class Split:
-    def __init__(self, df):
-        self.df = df.drop(columns=['team_name'])
+            if play['start']['team']['id'] == team_id:
+                team_stats['off'].append(play)
+            else:
+                team_stats['def'].append(play)
 
+    stats[team_id] = team_stats
 
-class Team:
-    def __init__(self, team_name):
-        self.df = stats_df.loc[stats_df['team_name'] == team_name]
-        self.offense = Split(self.df.xs('team', level=1))
-        self.defense = Split(self.df.xs('opp', level=1))
-
-
-class Teams:
-    def __init__(self, df):
-        self.df = df
-
-    def get_team(self, item):
-        return self.df.loc[self.df['team_name'] == item]
-
-    def rank_by_total(self, split, category):
-        return self.df.xs(split, level=1).groupby(['team_name'])[
-            category].sum().sort_values(ascending=False)
-
-    def rank_by_avg(self, split, category):
-        return self.df.xs(split, level=1).groupby(['team_name'])[
-            category].mean().sort_values(ascending=False)
-
-
-def select_category(split, category):
-    if split == 'off':
-        df = team_df.groupby(['team_name'])
-    else:
-        df = opp_df.groupby(['team_name'])
-
-    try:
-        return df[category]
-    except KeyError as exc:
-        raise exc
-
-
-def rank_teams_by_total(split, category):
-    series = select_category(split, category)
-
-    return series.sum().sort_values(ascending=False)
-
-
-def rank_teams_by_avg(split, category):
-    series = select_category(split, category)
-
-    return series.mean().sort_values(ascending=False)
-
-
-def rank_teams_by_median(split, category):
-    series = select_category(split, category)
-
-    return series.median().sort_values(ascending=False)
-
-
-def get_team_total(team_name, split, category):
-    series = select_category(split, category)
-
-    return series.sum().loc[team_name]
-
-
-def get_team_avg(team_name, split, category):
-    series = select_category(split, category)
-
-    return series.mean().loc[team_name]
+print(len(stats))
